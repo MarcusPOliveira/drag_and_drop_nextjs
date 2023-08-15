e671a92b845b6a1cbc75cf97925056a22ad87ba1
Marcus Paulo Oliveira <47436367+MarcusPOliveira@users.noreply.github.com>
2023-08-15 15:19:21 -0300

    drag and drop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,113 +1,240 @@
-import Image from 'next/image'
+"use client";
+import { useState } from "react";
+import {
+	DragDropContext,
+	Draggable,
+	DropResult,
+	Droppable,
+} from "react-beautiful-dnd";
+
+interface Item {
+	id: string;
+	content: string;
+	value: number;
+}
+
+interface Column {
+	id: string;
+	items: Item[];
+	title: string;
+	color: string;
+}
+
+const items: Item[] = [
+	{
+		id: "1",
+		content: "Item 1",
+		value: 100,
+	},
+	{
+		id: "2",
+		content: "Item 2",
+		value: 50,
+	},
+	{
+		id: "3",
+		content: "Item 3",
+		value: 75,
+	},
+];
+
+const initialColumns: Column[] = [
+	{
+		id: "Coluna_A",
+		items: items,
+		title: "Em negociação",
+		color: "#97e794",
+	},
+	{
+		id: "Coluna_B",
+		items: [],
+		title: "Pagamento efetuado",
+		color: "#8fafec",
+	},
+	{
+		id: "Coluna_C",
+		items: [],
+		title: "Pedido finalizado",
+		color: "#ad6ea8",
+	},
+];
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [columns, setColumns] = useState<Column[]>(initialColumns);
+	const [newItemCounter, setNewItemCounter] = useState<number>(4);
+	const [columnFilter, setColumnFilter] = useState<string>("");
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const onDragEnd = (result: DropResult) => {
+		// Identificando os IDs das colunas de origem e destino
+		const sourceColumnId = result.source.droppableId;
+		const destinationColumnId = result?.destination?.droppableId;
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+		// Encontrando os índices das colunas usando os IDs
+		const sourceColumnIndex = columns.findIndex(
+			(column) => column.id === sourceColumnId
+		);
+		const destinationColumnIndex = columns.findIndex(
+			(column) => column.id === destinationColumnId
+		);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+		// Verificando se as colunas de origem e destino foram encontradas
+		if (sourceColumnIndex === -1 || destinationColumnIndex === -1) {
+			return; // Se uma das colunas não for encontrada, sai da função
+		}
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+		// Obtendo as colunas de origem e destino usando os índices
+		const sourceColumn = columns[sourceColumnIndex];
+		const destinationColumn = columns[destinationColumnIndex];
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+		// Encontrando o item arrastado na coluna de origem
+		const draggedItem = sourceColumn.items.find(
+			(item) => item.id === result.draggableId
+		);
+
+		// Verificando se o item arrastado foi encontrado
+		if (!draggedItem) {
+			return; // Se o item não for encontrado, sai da função
+		}
+
+		// Criando uma cópia atualizada das colunas
+		const updatedColumns = [...columns];
+
+		// Atualizando o estado de acordo com a lógica de arrastar e soltar
+		if (sourceColumnIndex === destinationColumnIndex) {
+			// Caso o item seja solto na mesma coluna de origem
+			const filteredSourceItems = sourceColumn.items.filter(
+				(item) => item.id !== result.draggableId
+			);
+			filteredSourceItems.splice(
+				result?.destination?.index || 0,
+				0,
+				draggedItem
+			);
+			updatedColumns[sourceColumnIndex].items = filteredSourceItems;
+		} else {
+			// Caso o item seja solto em uma coluna de destino diferente
+			const filteredSourceItems = sourceColumn.items.filter(
+				(item) => item.id !== result.draggableId
+			);
+			const destinationItems = [...destinationColumn.items];
+			destinationItems.splice(result?.destination?.index || 0, 0, draggedItem);
+
+			updatedColumns[sourceColumnIndex].items = filteredSourceItems;
+			updatedColumns[destinationColumnIndex].items = destinationItems;
+		}
+
+		// Atualizando o estado com as colunas atualizadas
+		setColumns(updatedColumns);
+	};
+
+	const handleAddItem = () => {
+		// Crie um novo item com um ID único
+		const newItem = {
+			id: newItemCounter.toString(),
+			content: `Novo Item ${newItemCounter}`,
+			value: 50,
+		};
+
+		// Atualize o contador de novos itens
+		setNewItemCounter((prevCounter) => prevCounter + 1);
+
+		// Crie uma cópia atualizada das colunas
+		const updatedColumns = [...columns];
+
+		// Adicione o novo item à primeira coluna "Em negociação"
+		updatedColumns[0].items.push(newItem);
+
+		// Atualize o estado das colunas
+		setColumns(updatedColumns);
+	};
+
+	const calculateColumnTotal = (columnItems: Item[]): number => {
+		return columnItems.reduce((total, item) => total + item.value, 0);
+	};
+
+	const handleColumnFilterChange = (
+		event: React.ChangeEvent<HTMLInputElement>
+	) => {
+		setColumnFilter(event.target.value);
+	};
+
+	const filteredColumns = columnFilter
+		? columns.filter((column) =>
+				column.title.toLowerCase().includes(columnFilter.toLowerCase())
+		  )
+		: columns;
+
+	return (
+		<main className="flex md:py-16 md:px-24">
+			<div className="w-full h-auto font-mono flex flex-col rounded-md text-sm lg:flex bg-red-400 gap-4 p-4">
+				<div className="flex w-full">
+					<input
+						type="text"
+						placeholder="Filtrar por nome da coluna"
+						value={columnFilter}
+						onChange={handleColumnFilterChange}
+						className="w-56 p-2 border border-gray-300 rounded-md mr-2 text-black"
+					/>
+					<button
+						className="bg-blue-500 p-2 rounded-md"
+						onClick={handleAddItem}
+					>
+						Add item
+					</button>
+				</div>
+				<DragDropContext onDragEnd={onDragEnd}>
+					{filteredColumns.map((column) => (
+						<div>
+							<h1 className="w-[200px]">{column.title}</h1>
+							<Droppable
+								droppableId={column.id}
+								key={column.id}
+								direction="horizontal"
+							>
+								{(provided) => (
+									<div
+										className="flex items-center h-[120px] md:h-[100px] flex-row"
+										ref={provided.innerRef}
+									>
+										<div
+											className="flex flex-row w-full h-full overflow-x-auto rounded-md p-6 md:p-3 gap-4"
+											style={{ backgroundColor: column.color }}
+										>
+											{column.items.map((item, index) => (
+												<Draggable
+													draggableId={item.id}
+													index={index}
+													key={item.id}
+												>
+													{(provided) => (
+														<div
+															className="flex flex-col items-center justify-center text-center bg-gray-400 rounded-md min-w-[80px] md:w-[80px]"
+															style={{ ...provided.draggableProps.style }}
+															ref={provided.innerRef}
+															{...provided.dragHandleProps}
+															{...provided.draggableProps}
+														>
+															<p>{item.content}</p>
+															<p>{item.value},00</p>
+														</div>
+													)}
+												</Draggable>
+											))}
+											{provided.placeholder}
+										</div>
+										<div className="flex flex-col w-24 md:w-28 h-full rounded-md bg-gray-200 items-center justify-center">
+											<p className="text-black text-center">Valor total</p>
+											<p className="text-black">
+												R$ {calculateColumnTotal(column.items)},00
+											</p>
+										</div>
+									</div>
+								)}
+							</Droppable>
+						</div>
+					))}
+				</DragDropContext>
+			</div>
+		</main>
+	);
 }
